32e9edc42943bbb520208edb9e6fae1aeb3ee512
Daniel Saidi <daniel.saidi@gmail.com>
2023-05-26 14:38:08 +0200

    Add delay parameter to onboarding

--- Sources/DocumentKit/DocumentGroup+Onboarding.swift
+++ Sources/DocumentKit/DocumentGroup+Onboarding.swift
@@ -10,9 +10,24 @@ import SwiftUI
 
 public extension DocumentGroup {
 
+    /**
+     Present an onboarding sheet when the application starts.
+
+     The onboarding will only be presented once, after which
+     you can use the `UserDefaults` extensions to handle the
+     state of the onboarding. You can use different `id`s to
+     present different onboardings.
+
+     - Parameters:
+       - id: The onboarding ID, by default `nil`.
+       - store: The persistency store, by default `.standard`.
+       - delay: The delay before presenting the onboarding, by default `0.5`.
+       - content: The onboarding content.
+     */
     func onboardingSheet<Content: DocumentGroupModal>(
         id: String? = nil,
         store: UserDefaults? = nil,
+        delay: TimeInterval? = nil,
         @ViewBuilder content: @escaping () -> Content
     ) -> DocumentGroup {
         onboardingModal(
@@ -23,9 +38,24 @@ public extension DocumentGroup {
         )
     }
 
+    /**
+     Present an onboarding modal when the application starts.
+
+     The onboarding will only be presented once, after which
+     you can use the `UserDefaults` extensions to handle the
+     state of the onboarding. You can use different `id`s to
+     present different onboardings.
+
+     - Parameters:
+       - id: The onboarding ID, by default `nil`.
+       - store: The persistency store, by default `.standard`.
+       - delay: The delay before presenting the onboarding, by default `0.5`.
+       - content: The onboarding content.       
+     */
     func onboardingFullScreenCover<Content: DocumentGroupModal>(
         id: String? = nil,
         store: UserDefaults? = nil,
+        delay: TimeInterval? = nil,
         @ViewBuilder content: @escaping () -> Content
     ) -> DocumentGroup {
         onboardingModal(
@@ -42,13 +72,15 @@ private extension DocumentGroup {
     func onboardingModal<Content: DocumentGroupModal>(
         id: String? = nil,
         store: UserDefaults? = nil,
+        delay: TimeInterval? = nil,
         presentation: (Content) throws -> Void,
         @ViewBuilder content: @escaping () -> Content
     ) -> DocumentGroup {
         let store = store ?? .standard
+        let delay = delay ?? 0.5
         // store.resetDocumentGroupOnboardingState(for: id)
         if store.documentGroupOnboardingState(for: id) { return self }
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
             do {
                 try content().presentAsDocumentGroupFullScreenCover()
                 store.setDocumentGroupOnboardingState(to: true, for: id)

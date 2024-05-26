b871a91a065f0b985edb374ce2709da21244be9c
Daniel Saidi <daniel.saidi@gmail.com>
2024-05-26 23:35:49 +0200

    Replace main actor failing unit tests

---
+++ Tests/DocumentKitTests/DocumentKitTests.swift
@@ -0,0 +1,18 @@
+//
+//  DocumentKitTests.swift
+//  DocumentKit
+//
+//  Created by Daniel Saidi on 2023-05-26.
+//  Copyright © 2023-2024 Daniel Saidi. All rights reserved.
+//
+
+import DocumentKit
+import SwiftUI
+import XCTest
+
+final class DocumentKitTests: XCTestCase {
+
+    func testPlaceholder() {
+        XCTAssertEqual(1, 1)
+    }
+}
